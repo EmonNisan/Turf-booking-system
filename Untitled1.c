ed12b4af8d6b435fd9ee8e0bc8374df7e0dc8f9e
EmonNisan <153793688+EmonNisan@users.noreply.github.com>
2023-12-22 13:05:33 +0600

    Update Untitled1.c

--- Untitled1.c
+++ Untitled1.c
@@ -13,8 +13,11 @@
 
 char booking_username[100]=" ";
 char booking_username_cheak[100]=" ";
+char verify_ID[100]=" ";
+char For_empty[100]=" ";
 
-struct Turf {
+struct Turf
+{
     char name[MAX_NAME_LENGTH];
     char location[MAX_LOCATION_LENGTH];
     int timeSlots[Total_TIMESLOTS];
@@ -23,54 +26,61 @@ struct Turf {
 
 struct Turf turfs[Total_TURFS];
 
-float time[18]={8.00,8.45,9.30,10.15,11.00,11.45,12.30,13.15,14.00,14.45,15.30,16.15,17.00,17.45,18.30,19.15,20.00};
+float time[18]= {8.00,8.45,9.30,10.15,11.00,11.45,12.30,13.15,14.00,14.45,15.30,16.15,17.00,17.45,18.30,19.15,20.00};
 
-void displayTimeSlots(int timeSlots[], int choice12) { int a=0;
+void displayTimeSlots(int timeSlots[], int choice12)
+{
+    int a=0;
     printf("Available Time Slots:\n");
     for (int i = 0; i < Total_TIMESLOTS; ++i)
-        {
+    {
         printf("Slot %d: %s", i + 1, turfs[choice12-1].timeSlots[i] ? "Booked" : "Available");
 
-        for(int j=a;j<a+1;j++)
+        for(int j=a; j<a+1; j++)
         {
 
             printf("------Time--->(%.2f-%.2f)\n",time[j],time[j+1]);
 
         }
         a++;
-        }
+    }
     printf("\n");
 }
 
-   void  verify_account_write(char booking_username[])
-     {
-         FILE * file;
-         file=fopen("ID_verify.txt","a");
-         if(file==0)
-         {
-              file=fopen("ID_verify.txt","w");
-         }
-         else
-         {
-           fprintf(file,"%s\n",booking_username);
-         }
-     }
+void  verify_account_write(char booking_username[])
+{
+    FILE * file;
+    file=fopen("ID_verify.txt","a");
+    if(file==0)
+    {
+        file=fopen("ID_verify.txt","w");
+    }
+    else
+    {
+        fprintf(file,"%s\n",booking_username);
+    }
+}
 
 // Function to book a time slot
-void bookTimeSlot(int timeSlots[], int slotNumber,int choice11) {
-    if (slotNumber < 1 || slotNumber > Total_TIMESLOTS) {
+void bookTimeSlot(int timeSlots[], int slotNumber,int choice11)
+{
+    if (slotNumber < 1 || slotNumber > Total_TIMESLOTS)
+    {
         printf("Invalid time slot number.\n");
         return;
     }
 
-    if (turfs[choice11-1].timeSlots[slotNumber - 1] == 0) {
+    if (turfs[choice11-1].timeSlots[slotNumber - 1] == 0)
+    {
         turfs[choice11-1].timeSlots[slotNumber - 1] = 1;
 
         char verification[100]=" ";
         printf("\n---->Enter a verification code:");
         scanf("%s",verification);
-        strcat(booking_username,verification);
-        verify_account_write(booking_username);
+        strcat(verify_ID,booking_username);
+        strcat(verify_ID,verification);
+        verify_account_write(verify_ID);
+        strcpy(verify_ID,For_empty);
         printf("------Time slot %d booked successfully!-----\n", slotNumber);
         printf("----For confirm you book you have pay 30%% of slot payment with in 30 min otherwise your booking will be canceled----\n");
         printf("----You can pay by our Bkash Or Rocket account.Please use your website user name and verification code as a reference.----\n");
@@ -78,39 +88,48 @@ void bookTimeSlot(int timeSlots[], int slotNumber,int choice11) {
 
         // Write the booking information to the file
         FILE *file = fopen(FILENAME, "a");
-        if (file != NULL) {
+        if (file != NULL)
+        {
             fprintf(file, "%d %d\n",choice11, slotNumber);
             fclose(file);
-        } else {
+        }
+        else
+        {
             printf("Error writing to the file.\n");
         }
-    } else {
+    }
+    else
+    {
         printf("\t\t-------Sorry!Time slot %d is already booked.-------\n", slotNumber);
     }
 }
 
 // Function to display booked time slots
-void displayBookedSlots() {
+void displayBookedSlots()
+{
     printf("Booked Time Slots:\n");
 
     FILE *file = fopen(FILENAME, "r");
-    if (file != NULL) {
+    if (file != NULL)
+    {
 
-        int i,j,a=0,b=0;
-        while(!feof(file))
+        int i,j,a=-1,b=-1;
+        while(fscanf(file, "%d %d", &i, &j) == 2)
         {
-            fscanf(file,"%d %d",&i,&j);
             if(i!=a || j!=b)
             {
-            printf("Turf num: %d\t Slot num:%d\n",i,j);
-            i=a;
-            j=b;
+                printf("Turf num: %d\t Slot num:%d\n",i,j);
+                a=i;
+                b=j;
             }
         }
 
         fclose(file);
-    } else {
-        printf("Error reading from the file.\n");
+    }
+    else
+    {
+         FILE *file = fopen(FILENAME, "w");
+         fclose(file);                      //printf("Error reading from the file.\n");
     }
 
     printf("\n");
@@ -118,82 +137,87 @@ void displayBookedSlots() {
 }
 
 //Function to cancel booked time slots
-void cancleTimeSlot(int timeSlots[], int slotNumber,int choice1) {
+void cancleTimeSlot(int timeSlots[], int slotNumber,int choice1)
+{
     if (slotNumber < 1 || slotNumber > Total_TIMESLOTS)
-        {
+    {
         printf("Invalid time slot number.\n");
         return;
-        }
+    }
 
 
-        if (turfs[choice1-1].timeSlots[slotNumber - 1] == 1)
-        {
-                turfs[choice1-1].timeSlots[slotNumber - 1] = 0;
-        printf("Time slot %d cancel successfully!\n", slotNumber);
+    if (turfs[choice1-1].timeSlots[slotNumber - 1] == 1)
+    {
+        turfs[choice1-1].timeSlots[slotNumber - 1] = 0;
+        printf("\t\t------Time slot %d cancel successfully!------\n", slotNumber);
 
         FILE *file = fopen(FILENAME, "w");
-        if (file != NULL) {
-        for(int i=0;i<Total_TURFS;i++)
+        if (file != NULL)
         {
-            for( int j=0;j<Total_TIMESLOTS;j++)
+            for(int i=0; i<Total_TURFS; i++)
             {
-                if(turfs[i].timeSlots[j]==1)
+                for( int j=0; j<Total_TIMESLOTS; j++)
                 {
-                    fprintf(file,"%d %d\n",i+1 ,j+1);
+                    if(turfs[i].timeSlots[j]==1)
+                    {
+                        fprintf(file,"%d %d\n",i+1,j+1);
+                    }
                 }
             }
         }
     }
-        }
-    else {
-        printf("Time slot %d is already empty.\n", slotNumber);
+    else
+    {
+        printf("\t\tTime slot %d is already empty.\n", slotNumber);
     }
 }
 
 //Function to verify your id
-   verify_account_read(int timeSlots[], int slotNumber,int choice1)
-     {
-         int cheak=0;
-         char verify[100]=" ";
-         printf("Enter your verification code: ");
-         scanf("%s",verify);
-         strcat(booking_username,verify);
+verify_account_read(int timeSlots[], int slotNumber,int choice1)
+{
+    int cheak=0;
+    char verify[100]=" ";
+    printf("Enter your verification code: ");
+    scanf("%s",verify);
+    strcat(booking_username,verify);
 
-         FILE * file;
-         file=fopen("ID_verify.txt","r");
-         if(file==0)
-         {
-              file=fopen("ID_verify.txt","w");
-         }
-         else
-         {
-             while(!feof(file))
-                   {
-                       fscanf(file,"%s",booking_username_cheak);
+    FILE * file;
+    file=fopen("ID_verify.txt","r");
+    if(file==0)
+    {
+        file=fopen("ID_verify.txt","w");
+    }
+    else
+    {
+        while(!feof(file))
+        {
+            fscanf(file,"%s",booking_username_cheak);
 
-             if(strcmp(booking_username_cheak,booking_username)==0)
-                {
-                    printf("Id Verified\n");
-                    cheak=1;
-                    break;
-                }
-                   }
-
-         }
-
-            if(cheak==1){
-          cancleTimeSlot(timeSlots, slotNumber,choice1);
-          }
-
-            else
+            if(strcmp(booking_username_cheak,booking_username)==0)
             {
-                printf("\t\t------>You input wrong verification code.<------\n");
-
+                printf("Id Verified\n");
+                cheak=1;
+                break;
             }
-     }
+        }
 
-     //Function for bookings menu
-book() {
+    }
+
+    if(cheak==1)
+    {
+        cancleTimeSlot(timeSlots, slotNumber,choice1);
+    }
+
+    else
+    {
+        printf("\t\t------>You input wrong verification code.<------\n");
+
+    }
+}
+
+//Function for bookings menu
+book()
+{
     int choice1;
     int choice15;
     int timeSlots[Total_TIMESLOTS];// = {0}; // 0 represents an available slot, 1 represents a booked slot
@@ -202,30 +226,30 @@ book() {
     scanf("%d", &choice1);
     // Read existing bookings from the file
     FILE *file = fopen(FILENAME, "r");
-    if (file != NULL) {
-        int slot;
+    if (file != NULL)
+    {
+          int slot;
 
-        while (!feof(file)) {
-           // if (slot >= 1 && slot <= Total_TIMESLOTS)
-                {
-                        fscanf(file, "%d %d \n",&choice15, &slot);
-                turfs[choice15-1].timeSlots[slot - 1] = 1;
-
-            }
-        }
-
-
-        fclose(file);
+    // Use fscanf's return value to check for successful reading
+    while (fscanf(file, "%d %d", &choice15, &slot) == 2)
+    {
+        turfs[choice15 - 1].timeSlots[slot - 1] = 1;
     }
-    else{
-            printf("file not found");
+
+    fclose(file);
+
+    }
+    else
+    {
+        printf("file not found");
 
     }
 
 
     int choice2, slotNumber;
 
-    do {
+    do
+    {
         printf("Booking Menu:\n ");
         printf("\t1. Display available time slots\n");
         printf("\t2. Book a time slot\n");
@@ -236,39 +260,43 @@ book() {
         printf("Enter your choice: ");
         scanf("%d", &choice2);
 
-        switch (choice2) {
-            case 1:
-                displayTimeSlots(timeSlots,choice1);
-                break;
-            case 2:
-                printf("Enter the time slot number you want to book: ");
-                scanf("%d", &slotNumber);
-                bookTimeSlot(timeSlots, slotNumber,choice1);
-                break;
-            case 3:
-                printf("Enter the time slot number you want to cancel: ");
-                scanf("%d", &slotNumber);
-                 verify_account_read(timeSlots, slotNumber,choice1);
-                break;
-            case 4:
-                displayBookedSlots();
-                break;
-            case 5:
-                printf("Exiting the program.\n");
-                break;
-            default:
-                printf("Invalid choice. Please try again.\n");
-                break;
+        switch (choice2)
+        {
+        case 1:
+            displayTimeSlots(timeSlots,choice1);
+            break;
+        case 2:
+            printf("\nEnter the time slot number you want to book: ");
+            scanf("%d", &slotNumber);
+            bookTimeSlot(timeSlots, slotNumber,choice1);
+            break;
+        case 3:
+            printf("\nEnter the time slot number you want to cancel: ");
+            scanf("%d", &slotNumber);
+            verify_account_read(timeSlots, slotNumber,choice1);
+            break;
+        case 4:
+            displayBookedSlots();
+            break;
+        case 5:
+            printf("Exiting the program.\n");
+            break;
+        default:
+            printf("Invalid choice. Please try again.\n");
+            break;
         }
 
-    } while (choice2 != 5);
+    }
+    while (choice2 != 5);
 
 
 }
- //Fucntion for all turfs name and location and slot price
-void displayAllTurfs() {
+   //Fucntion for all turfs name and location and slot price
+void displayAllTurfs()
+{
     printf("\n\t\t\t\t---->Available Turfs<----\n\n");
-    for (int i = 0; i < Total_TURFS; ++i) {
+    for (int i = 0; i < Total_TURFS; ++i)
+    {
         printf("\n%d. Turf name: %s, Location: %s\n", i + 1, turfs[i].name, turfs[i].location);
         if(i==0)
         {
@@ -313,16 +341,19 @@ void displayAllTurfs() {
     }
 }
 
-void readTurfDataFromFile() {
-  // printf("jhdvs");
+void readTurfDataFromFile()
+{
+    // printf("jhdvs");
     FILE *file = fopen("turfs.txt", "r");
 
-    if (file == NULL) {
-        printf("Error opening file.\n");
+    if (file == NULL)
+    {
+        FILE *file = fopen("turfs.txt", "w");
         return;
     }
 
-    for (int i = 0; i < Total_TURFS; ++i) {
+    for (int i = 0; i < Total_TURFS; ++i)
+    {
         fscanf(file, "%s %s\n", turfs[i].name, turfs[i].location);
 
     }
@@ -333,18 +364,21 @@ void readTurfDataFromFile() {
 
 
 
-    void writeTurfDataToFile() {
+void writeTurfDataToFile()
+{
     FILE *file = fopen("turfs.txt", "w");
 
-    if (file == NULL) {
+    if (file == NULL)
+    {
         printf("Error opening file.\n");
         return;
     }
 
-    for (int i = 0; i < Total_TURFS; ++i) {
+    for (int i = 0; i < Total_TURFS; ++i)
+    {
 
 
-       // scanf("%s %s %s %d\n", turfs[i].name, turfs[i].location, turfs[i].timeSlots, turfs[i].isBooked);
+        // scanf("%s %s %s %d\n", turfs[i].name, turfs[i].location, turfs[i].timeSlots, turfs[i].isBooked);
 
         fprintf(file, "%s %s \n", turfs[i].name, turfs[i].location);
     }
@@ -352,61 +386,81 @@ void readTurfDataFromFile() {
     fclose(file);
 }
 
-  struct login{
+struct login
+{
     char first_name[100];
     char last_name[100];
     char username[100];
     char password[100];
 };
+
+int user_name_exits(char *username)
+{
+    FILE*log;
+    log=fopen("login12.txt", "r");
+    struct login f;
+    while(fread(&f,sizeof(f),1,log))
+    {
+        if(strcmp(f.username,username)==0)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
 //login function
-   login()
-     {
+login()
+{
 
-         int cheak=0;
-         char username[100];
-         char password[100];
+    int cheak=0;
+    char username[100];
+    char password[100];
 
-         FILE*log;
-         log=fopen("login12.txt", "r");
-         struct login l;
-         printf("Enter your USER ID: ");
-         scanf("%s", username);
-         printf("\n\nEnter your password: ");
-         scanf("%s", password);
-
-
+    FILE*log;
+    log=fopen("login12.txt", "r");
+    struct login l;
+    printf("Enter your USER ID: ");
+    scanf("%s", username);
+    printf("\n\nEnter your password: ");
+    scanf("%s", password);
 
 
-         while(fread(&l,sizeof(l),1,log))
+
+
+    while(fread(&l,sizeof(l),1,log))
+    {
+        if(strcmp(username,l.username)==0 && strcmp(password,l.password)==0)
+        {
+            printf("\n\t\t------Succesfully login------");
+            cheak=1;
+            strcpy(booking_username,username);
+            break;
+        }
+        /* else if(feof(log))
          {
-             if(strcmp(username,l.username)==0 && strcmp(password,l.password)==0)
-             {
-                 printf("\n\t\t------S1uccesfully login------");
-                 cheak=1;
-                 strcpy(booking_username,username);
-                 break;
-             }
-            /* else if(feof(log))
-             {
-                  printf("\nPlease enter correct USER ID and password");
+              printf("\nPlease enter correct USER ID and password");
 
-             }*/
+         }*/
 
-         }
-           int choice;
+    }
+    int choice;
 
-     //Menu option after login
-           if(cheak==1){
-    do {
-        printf("\nMenu:\n");
-        printf("\t\t1. Display all turfs\n");
-        printf("\t\t2. Book or cancel turf\n");
-        printf("\t\t3. Exit\n");
+    //Menu option after login
+    if(cheak==1)
+    {
+        do
+        {
+            printf("\nMenu:\n");
+            printf("\t\t1. Display all turfs\n");
+            printf("\t\t2. Book or cancel turf\n");
+            printf("\t\t3. Exit\n");
 
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
+            printf("Enter your choice: ");
+            scanf("%d", &choice);
 
-        switch (choice) {
+            switch (choice)
+            {
             case 1:
                 displayAllTurfs();
                 break;
@@ -419,46 +473,59 @@ void readTurfDataFromFile() {
             default:
                 printf("Invalid choice. Please try again.\n");
                 break;
+            }
         }
-    } while (choice != 3);
+        while (choice != 3);
 
-    writeTurfDataToFile();
+        writeTurfDataToFile();
 
 
-    printf("\n\t\t\t--------WELLCOME--------");
-    getch();
-         }
+        printf("\n\t\t\t--------WELLCOME--------");
+        getch();
+    }
 
-        else if(cheak==0)
-         {
-              printf("\nPlease enter correct USER ID and password");
-         }
-        }
+    else if(cheak==0)
+    {
+        printf("\nPlease enter correct USER ID and password");
+    }
+}
 // Function for create id
-        create()
-     {
-         FILE * log;
-         log = fopen("login12.txt", "a");
-         struct login l;
+create()
+{
+    FILE * log;
+    log = fopen("login12.txt", "a");
+    struct login l;
 
-         printf("Enter your first name: ");
-         scanf("%s", l.first_name);
-         printf("\n\nEnter your last name: ");
-         scanf("%s", l.last_name);
-         printf("\n\nEnter a username: ");
-         scanf("%s", l.username);
-         printf("\n\nEnter password: ");
-         scanf("%s", l.password);
-         fwrite(&l,sizeof(l),1,log);
+    printf("Enter your first name: ");
+    scanf("%s", l.first_name);
+    printf("\n\nEnter your last name: ");
+    scanf("%s", l.last_name);
 
-         fclose(log);
+    do
+    {
+        printf("\n\nEnter a username: ");
+        scanf("%s", l.username);
+        if(user_name_exits(l.username))
+        {
+            printf("\nThis user is already in use. Try another.");
+        }
+    }
+    while(user_name_exits(l.username));
 
-         printf("\n\nYour user name is USER ID ");
-         printf("\n\nNow login with your USER ID and password");
-         printf("\n\n------Press any key to continue------\n\n");
-         getch();
-         login();
-     }
+
+    printf("\n\nEnter password: ");
+    scanf("%s", l.password);
+    fwrite(&l,sizeof(l),1,log);
+
+    fclose(log);
+
+    printf("\n\nYour user name is USER ID ");
+    printf("\n\nNow login with your USER ID and password");
+    printf("\n\n------Press any key to continue------\n\n");
+    getch();
+    login();
+}
+
 
 
 
@@ -468,25 +535,23 @@ int main()
     readTurfDataFromFile();
 
     int choice;
-     printf("------Press '1' for CREATE ID------ \n\n------Press '2' for LOGIN ID------\n\n");
-         scanf("%d",&choice);
-         if(choice==1)
-         {
-             create();
-         }
-         else if(choice==2)
-         {
-             login();
-         }
-         else
-         {
-             printf("------>You input wrong number<-----");
-         }
+    printf("------Press '1' for CREATE ID------ \n\n------Press '2' for LOGIN ID------\n\n");
+    scanf("%d",&choice);
+    if(choice==1)
+    {
+        create();
+    }
+    else if(choice==2)
+    {
+        login();
+    }
+    else
+    {
+        printf("------>You input wrong number<-----");
+    }
 
 
     return 0;
 
 
 }
-
-
